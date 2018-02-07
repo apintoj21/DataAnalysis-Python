31f13e2f44cf0b6cf1f2bb783ac162a51e368d2b
apintoj21 <rocia.fernandes@gmail.com>
2018-02-07 19:02:00 +0530

    update

--- BuildingDatasets.py
+++ BuildingDatasets.py
@@ -1,12 +1,5 @@
-import Quandl
+import pandas as pd
 
-# Not necessary, I just do this so I do not show my API key.
-api_key = open('quandlapikey.txt','r').read()
-
-df = Quandl.get("FMAC/HPI_TX", authtoken=api_key)
-
-print(df.head())
-print("_________________________________________________________")
 fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
 print(fiddy_states)
 print("_________________________________________________________")
