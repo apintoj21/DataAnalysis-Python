edf68077ee1caeb829c702eaa0af3511144a21a0
apintoj21 <35739958+apintoj21@users.noreply.github.com>
2018-02-07 19:03:39 +0530

    Update BuildingDatasets.py

--- BuildingDatasets.py
+++ BuildingDatasets.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
-fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
-print(fiddy_states)
-print("_________________________________________________________")
-print(fiddy_states[0])
-print("_________________________________________________________")
-for abbv in fiddy_states[0][0][1:]:
-    print(abbv)
-print("_________________________________________________________")
-for abbv in fiddy_states[0][0][1:]:
-    #print(abbv)
-    print("FMAC/HPI_"+str(abbv))
-print("_________________________________________________________")
+if __name__ == '__main__':
+    fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
+    print(fiddy_states)
+    print("_________________________________________________________")
+    print(fiddy_states[0])
+    print("_________________________________________________________")
+    for abbv in fiddy_states[0][0][1:]:
+        print(abbv)
+    print("_________________________________________________________")
+    for abbv in fiddy_states[0][0][1:]:
+        #print(abbv)
+        print("FMAC/HPI_"+str(abbv))
+    print("_________________________________________________________")
 
